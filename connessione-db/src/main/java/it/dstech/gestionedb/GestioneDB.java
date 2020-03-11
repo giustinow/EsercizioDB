a243604ac7e40c6537260ecbb855dc3a8746ce6f
giustinow <justinebiscochor@gmail.com>
2020-03-11 15:59:27 +0100

    inizio del nuovo progetto con la creazione del main e del pom.xml

---
+++ connessione-db/src/main/java/it/dstech/gestionedb/GestioneDB.java
@@ -0,0 +1,14 @@
+package it.dstech.gestionedb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class GestioneDB {
+	public static void main(String[] args){
+
+	}
+}
+	
