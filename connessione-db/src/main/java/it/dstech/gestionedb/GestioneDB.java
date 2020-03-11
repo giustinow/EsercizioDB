3ea7a490e318dc1174df55000c65fc15c93448c1
giustinow <justinebiscochor@gmail.com>
2020-03-11 20:09:25 +0100

    Aggiunta del case 2 e sistemato il case 1

--- connessione-db/src/main/java/it/dstech/gestionedb/GestioneDB.java
+++ connessione-db/src/main/java/it/dstech/gestionedb/GestioneDB.java
@@ -11,15 +11,17 @@ import java.util.regex.Pattern;
 
 public class GestioneDB {
 	public static final String REGEX_COUNTRY_CODE = "[A-Z]{3}";
+
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Scanner input = new Scanner(System.in);
-		Class.forName("com.mysql.cj.jdbc.Driver"); // in questo punto carichia nella JVM in esecuzione la nostra libreria 
-		String password ="pavillion"; // la vostra password
+		Class.forName("com.mysql.cj.jdbc.Driver"); // in questo punto carichia nella JVM in esecuzione la nostra
+													// libreria
+		String password = "pavillion"; // la vostra password
 		String username = "root"; // la vostra username
 		String url = "jdbc:mysql://localhost:3306/world?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=UTC&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true&useSSL=false";
 		Connection connessione = DriverManager.getConnection(url, username, password);
 		Statement statement = connessione.createStatement();
-		
+
 //		ResultSet risultatoQuery = statement.executeQuery("select * from city where CountryCode =  \"ITA\" ;");
 //		while(risultatoQuery.next()) {
 //			int id = risultatoQuery.getInt(1);
@@ -27,33 +29,56 @@ public class GestioneDB {
 //			int pop = risultatoQuery.getInt(5);
 //			System.out.println(id + " " + nome + " - " + pop);
 //		}
-		while(true) {
+		while (true) {
 			menu();
 			int scelta = input.nextInt();
 			input.nextLine();
 			switch (scelta) {
-			case 1:{
+			case 1: {
+				boolean verifica = false;
 				String countryCode = verificaCountryCode(input);
-				ResultSet risultatoQuery = statement.executeQuery("select * from world.city where CountryCode =  \" countryCode \" ;");
-				while(risultatoQuery.next()) {
+				ResultSet risultatoQuery = statement
+						.executeQuery("select * from city where CountryCode =  \"" + countryCode + "\"");
+				while (risultatoQuery.next()) {
 					String nome = risultatoQuery.getString("Name");
 					String codice = risultatoQuery.getString("CountryCode");
 					System.out.println(nome + "  " + codice);
 				}
+				if(!verifica) {
+					System.out.println("Questa Country Code non esiste");
+				}
 				break;
 			}
-			case 2:{
+			case 2: {
+				boolean verifica = false;
+				System.out.println("Inserisci una nazione di cui vuoi vederne la città più popolosa");
+				String nazione = input.nextLine();
+				ResultSet risultatoQuery = statement.executeQuery(
+						"select country.Code, city.Name, country.Continent, city.District, city.Population from world.country inner join city on world.country.Code = world.city.CountryCode where country.Name = \"" + nazione + "\"" + "limit 0,1");
+				while (risultatoQuery.next()) {
+					String code = risultatoQuery.getString("Code");
+					String nomeCitta = risultatoQuery.getString("Name");
+					String continent = risultatoQuery.getString("Continent");
+					String district = risultatoQuery.getString("District");
+					int population = risultatoQuery.getInt("Population");
+					System.out.println(code + " " + nomeCitta + " " + continent + " " + district + " "	+ population);
+					verifica = true;
+				}
+				if(!verifica) {
+					System.out.println("Questa nazione non esiste");
+				}
 				break;
 			}
-			case 3:{
+			case 3: {
 				break;
 			}
 			default:
 				break;
 			}
 		}
-		
+
 	}
+
 	public static String verificaCountryCode(Scanner input) {
 		boolean verifica = true;
 		while (verifica) {
@@ -70,6 +95,7 @@ public class GestioneDB {
 		}
 		return null;
 	}
+
 	public static void menu() {
 		System.out.println("*********************************************************");
 		System.out.println("Fai la tua scelta");
@@ -78,6 +104,7 @@ public class GestioneDB {
 		System.out.println("3. Stato con estensione territoriale maggiore");
 		System.out.println("*********************************************************");
 	}
+
 	public static boolean checkData(String regex, String data) {
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(data);
