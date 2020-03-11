654cc5e8650c907f071b665f3657d2bfd8b465ba
giustinow <justinebiscochor@gmail.com>
2020-03-11 20:36:05 +0100

    Completato il terzo case, sistemato i metodi e migliorato il codice

--- connessione-db/src/main/java/it/dstech/gestionedb/GestioneDB.java
+++ connessione-db/src/main/java/it/dstech/gestionedb/GestioneDB.java
@@ -14,21 +14,13 @@ public class GestioneDB {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Scanner input = new Scanner(System.in);
-		Class.forName("com.mysql.cj.jdbc.Driver"); // in questo punto carichia nella JVM in esecuzione la nostra
-													// libreria
-		String password = "pavillion"; // la vostra password
-		String username = "root"; // la vostra username
+		Class.forName("com.mysql.cj.jdbc.Driver"); 
+		String password = "pavillion"; 
+		String username = "root"; 
 		String url = "jdbc:mysql://localhost:3306/world?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=UTC&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true&useSSL=false";
 		Connection connessione = DriverManager.getConnection(url, username, password);
 		Statement statement = connessione.createStatement();
 
-//		ResultSet risultatoQuery = statement.executeQuery("select * from city where CountryCode =  \"ITA\" ;");
-//		while(risultatoQuery.next()) {
-//			int id = risultatoQuery.getInt(1);
-//			String nome = risultatoQuery.getString("Name");
-//			int pop = risultatoQuery.getInt(5);
-//			System.out.println(id + " " + nome + " - " + pop);
-//		}
 		while (true) {
 			menu();
 			int scelta = input.nextInt();
@@ -37,13 +29,7 @@ public class GestioneDB {
 			case 1: {
 				boolean verifica = false;
 				String countryCode = verificaCountryCode(input);
-				ResultSet risultatoQuery = statement
-						.executeQuery("select * from city where CountryCode =  \"" + countryCode + "\"");
-				while (risultatoQuery.next()) {
-					String nome = risultatoQuery.getString("Name");
-					String codice = risultatoQuery.getString("CountryCode");
-					System.out.println(nome + "  " + codice);
-				}
+				countryCodeQuery(statement, countryCode);
 				if(!verifica) {
 					System.out.println("Questa Country Code non esiste");
 				}
@@ -53,23 +39,20 @@ public class GestioneDB {
 				boolean verifica = false;
 				System.out.println("Inserisci una nazione di cui vuoi vederne la città più popolosa");
 				String nazione = input.nextLine();
-				ResultSet risultatoQuery = statement.executeQuery(
-						"select country.Code, city.Name, country.Continent, city.District, city.Population from world.country inner join city on world.country.Code = world.city.CountryCode where country.Name = \"" + nazione + "\"" + "limit 0,1");
-				while (risultatoQuery.next()) {
-					String code = risultatoQuery.getString("Code");
-					String nomeCitta = risultatoQuery.getString("Name");
-					String continent = risultatoQuery.getString("Continent");
-					String district = risultatoQuery.getString("District");
-					int population = risultatoQuery.getInt("Population");
-					System.out.println(code + " " + nomeCitta + " " + continent + " " + district + " "	+ population);
-					verifica = true;
-				}
+				verifica = countryNameQuery(statement, verifica, nazione);
 				if(!verifica) {
 					System.out.println("Questa nazione non esiste");
 				}
 				break;
 			}
 			case 3: {
+				boolean verifica = false;
+				System.out.println("Inserisci una forma di governo di cui vuoi vedere la nazione con l'estensione maggiore");
+				String nazione = input.nextLine();
+				verifica = countrySizeQuery(statement, verifica, nazione);
+				if(!verifica) {
+					System.out.println("Questa forma di governo non esiste");
+				}
 				break;
 			}
 			default:
@@ -77,6 +60,45 @@ public class GestioneDB {
 			}
 		}
 
+	}
+
+	public static boolean countrySizeQuery(Statement statement, boolean verifica, String nazione) throws SQLException {
+		ResultSet risultatoQuery = statement.executeQuery(
+				"select country.code, country.name, country.SurfaceArea, country.GovernmentForm from world.country where country.GovernmentForm = \"" + nazione + "\"" + "order by country.SurfaceArea DESC limit 0,1");
+		while (risultatoQuery.next()) {
+			String code = risultatoQuery.getString("Code");
+			String nomeCitta = risultatoQuery.getString("Name");
+			int surfaceArea = risultatoQuery.getInt("SurfaceArea");
+			String govermentForm = risultatoQuery.getString("GovernmentForm");
+			System.out.println(code + " " + nomeCitta + " " + surfaceArea + " " + govermentForm);
+			verifica = true;
+		}
+		return verifica;
+	}
+
+	public static boolean countryNameQuery(Statement statement, boolean verifica, String nazione) throws SQLException {
+		ResultSet risultatoQuery = statement.executeQuery(
+				"select country.Code, city.Name, country.Continent, city.District, city.Population from world.country inner join city on world.country.Code = world.city.CountryCode where country.Name = \"" + nazione + "\"" + "limit 0,1");
+		while (risultatoQuery.next()) {
+			String code = risultatoQuery.getString("Code");
+			String nomeCitta = risultatoQuery.getString("Name");
+			String continent = risultatoQuery.getString("Continent");
+			String district = risultatoQuery.getString("District");
+			int population = risultatoQuery.getInt("Population");
+			System.out.println(code + " " + nomeCitta + " " + continent + " " + district + " "	+ population);
+			verifica = true;
+		}
+		return verifica;
+	}
+
+	public static void countryCodeQuery(Statement statement, String countryCode) throws SQLException {
+		ResultSet risultatoQuery = statement
+				.executeQuery("select * from city where CountryCode =  \"" + countryCode + "\"");
+		while (risultatoQuery.next()) {
+			String nome = risultatoQuery.getString("Name");
+			String codice = risultatoQuery.getString("CountryCode");
+			System.out.println(nome + "  " + codice);
+		}
 	}
 
 	public static String verificaCountryCode(Scanner input) {
